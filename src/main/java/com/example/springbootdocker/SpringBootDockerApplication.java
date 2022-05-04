e3c735870911af540dbeff2a297548a51e36afc7
Fabian Gutiérrez Escamilla <fabian.gti@hotmail.com>
2022-05-04 13:40:32 -0500

    Udating code Kubernetes

--- src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
+++ src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
@@ -2,8 +2,12 @@ package com.example.springbootdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class SpringBootDockerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/springbootdocker/controller/BootController.java
+++ src/main/java/com/example/springbootdocker/controller/BootController.java
@@ -2,17 +2,22 @@ package com.example.springbootdocker.controller;
 
 import java.time.LocalDateTime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BootController {
+	
+	private static Logger logger = LoggerFactory.getLogger(BootController.class);
 
 	@GetMapping("/hellgate/{name}")
 	public String getHome(@PathVariable("name") String name) {
+		logger.info("Boot controller {}", name);
 		String value = "Hell ".concat(name).concat(" Response ").concat(LocalDateTime.now().toString());
-		System.out.println(value);
+		logger.info("Boot response {}", value);
 		return value;
 	}
 
